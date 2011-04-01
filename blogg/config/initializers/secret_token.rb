073490552d7ad79e64964976cdddf3eaf428c0a3
Kamil Rzewudzki <kurczakov@gmail.com>
2011-04-01 12:37:57 +0200

    Pierwszy

---
+++ blogg/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogg::Application.config.secret_token = 'b53ac6b2a046148dd775d310115c17e85f1963a361782e24005fa945a0a493eec584be5eaad08ae8e6bd4ebd44354be414751f0bc26f573f06b02e52c7ad66f2'
